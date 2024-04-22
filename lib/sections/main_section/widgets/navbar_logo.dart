f57038411c21788354d52056298d953236bf8cf4
mohfauzislamet <mohfauzislamet@gmail.com>
2024-04-22 15:26:32 +0700

    tes cek

--- lib/sections/main_section/widgets/navbar_logo.dart
+++ lib/sections/main_section/widgets/navbar_logo.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:zi_dev_web/configs/app_theme.dart';
 import 'package:zi_dev_web/configs/app_typography.dart';
-import 'package:zi_dev_web/provider/app_provider.dart';
 
 class NavBarLogo extends StatelessWidget {
   const NavBarLogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final appProvider = Provider.of<AppProvider>(context);
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
         Text(
           "< ",
           style: AppText.b1!.copyWith(
-            color: appProvider.isDark ? Colors.white : Colors.black,
+            color: AppTheme.c!.text!,
           ),
         ),
         Text(
           "zi_dev",
           style: AppText.b1b!.copyWith(
             fontFamily: 'Montserrat',
-            color: appProvider.isDark ? Colors.white : Colors.black,
+            color: AppTheme.c!.text!,
           ),
         ),
         Text(
           MediaQuery.of(context).size.width >= 1000 ? " />\t\t" : " />",
           style: AppText.b1!.copyWith(
-            color: appProvider.isDark ? Colors.white : Colors.black,
+            color: AppTheme.c!.text!,
           ),
         )
       ],
