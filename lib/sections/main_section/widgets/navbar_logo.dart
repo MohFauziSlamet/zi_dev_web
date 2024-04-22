988c7d57a368bb8ea7adfc727e62c001de9e2041
mohfauzislamet <mohfauzislamet@gmail.com>
2024-04-22 14:31:07 +0700

    check color font di navbar , ketika darkMode

--- lib/sections/main_section/widgets/navbar_logo.dart
+++ lib/sections/main_section/widgets/navbar_logo.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:zi_dev_web/configs/app_typography.dart';
+import 'package:zi_dev_web/provider/app_provider.dart';
 
 class NavBarLogo extends StatelessWidget {
   const NavBarLogo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final appProvider = Provider.of<AppProvider>(context);
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
         Text(
           "< ",
-          style: AppText.b1,
+          style: AppText.b1!.copyWith(
+            color: appProvider.isDark ? Colors.black : Colors.white,
+          ),
         ),
         Text(
           "zi_dev",
           style: AppText.b1b!.copyWith(
             fontFamily: 'Montserrat',
+            color: appProvider.isDark ? Colors.black : Colors.white,
           ),
         ),
         Text(
           MediaQuery.of(context).size.width >= 1000 ? " />\t\t" : " />",
-          style: AppText.b1,
+          style: AppText.b1!.copyWith(
+            color: appProvider.isDark ? Colors.black : Colors.white,
+          ),
         )
       ],
     );
